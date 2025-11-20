7f639289810c53094ca08e1912f601b90fc9e09f
Vadim Rutkovsky <vadim@vrutkovs.eu>
2025-11-20 13:14:37 +0100

    Generate allure report

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -7,7 +7,11 @@ import (
 
 	terratesting "github.com/gruntwork-io/terratest/modules/testing"
 
+	"github.com/Moon1706/ginkgo2allure/pkg/convert"
+	fmngr "github.com/Moon1706/ginkgo2allure/pkg/convert/file_manager"
+	"github.com/Moon1706/ginkgo2allure/pkg/convert/parser"
 	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2/types"
 	. "github.com/onsi/gomega"
 )
 
@@ -46,3 +50,16 @@ type myTestingT struct {
 func (mt *myTestingT) Name() string {
 	return "[TerraTest]"
 }
+
+var _ = ReportAfterSuite("allure report", func(report types.Report) {
+	allureReports, err := convert.GinkgoToAllureReport([]types.Report{report}, parser.NewDefaultParser, parser.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	fileManager := fmngr.NewFileManager("./allure-results")
+	errs := convert.PrintAllureReports(allureReports, fileManager)
+	if len(errs) > 0 {
+		panic(errs)
+	}
+})

89053d366c52c9dee9ca81e7fbe9369f443466c7
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-01-26 09:19:38 +0100

    vmcluster: run multiple tests

--- tests/vmcluster_test/vmcluster_test.go
+++ tests/vmcluster_test/vmcluster_test.go
@@ -201,6 +201,95 @@ var _ = Describe("VMCluster test", Label("vmcluster"), func() {
 			value, err = tenantTwoProm.VectorValue(ctx, "foo_2")
 			require.EqualError(t, err, "no data returned")
 			require.Equal(t, value, model.SampleValue(0))
+
+			By("Verifying data can be retrieved via multitenant URL")
+			multitenantSelectURL := fmt.Sprintf("%s/select/multitenant/prometheus", consts.VMSelectUrl(vmNamespace))
+			multitenantProm, err := promquery.NewPrometheusClient(multitenantSelectURL)
+			multitenantProm.Start = overwatch.Start
+			require.NoError(t, err)
+			value, err = multitenantProm.VectorValue(ctx, "foo_2")
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(1))
+			value, err = multitenantProm.VectorValue(ctx, "bar_2")
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(5))
+		})
+
+		It("should accept data via multitenant URL", Label("gke", "id=16c08934-9e25-45ed-a94b-4fbbbe3170ef"), func(ctx context.Context) {
+			multitenantInsertURL := fmt.Sprintf("http://%s/insert/multitenant/prometheus/api/v1/write", consts.VMInsertHost(vmNamespace))
+
+			By("Inserting data into tenant 0")
+			ts := remotewrite.GenTimeSeries("foo", 10, 1)
+			for i, item := range ts {
+				ts[i].Labels = append(item.Labels, prompb.Label{
+					Name: "vm_account_id", Value: "0",
+				})
+			}
+			err := remotewrite.RemoteWrite(c, ts, multitenantInsertURL)
+			require.NoError(t, err)
+
+			By("Inserting data into tenant 1")
+			ts = remotewrite.GenTimeSeries("bar", 10, 5)
+			for i, item := range ts {
+				ts[i].Labels = append(item.Labels, prompb.Label{
+					Name: "vm_account_id", Value: "1",
+				})
+			}
+			err = remotewrite.RemoteWrite(c, ts, multitenantInsertURL)
+			require.NoError(t, err)
+
+			time.Sleep(30 * time.Second)
+
+			By("Verifying data is not mixed")
+			tenantOneSelectURL := fmt.Sprintf("%s/select/0/prometheus", consts.VMSelectUrl(vmNamespace))
+			tenantOneProm, err := promquery.NewPrometheusClient(tenantOneSelectURL)
+			tenantOneProm.Start = overwatch.Start
+			require.NoError(t, err)
+			value, err := tenantOneProm.VectorValue(ctx, "foo_2")
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(1))
+			value, err = tenantOneProm.VectorValue(ctx, "bar_2")
+			require.EqualError(t, err, "no data returned")
+			require.Equal(t, value, model.SampleValue(0))
+
+			tenantTwoSelectURL := fmt.Sprintf("%s/select/1/prometheus", consts.VMSelectUrl(vmNamespace))
+			tenantTwoProm, err := promquery.NewPrometheusClient(tenantTwoSelectURL)
+			tenantTwoProm.Start = overwatch.Start
+			require.NoError(t, err)
+			value, err = tenantTwoProm.VectorValue(ctx, "bar_2")
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(5))
+			value, err = tenantTwoProm.VectorValue(ctx, "foo_2")
+			require.EqualError(t, err, "no data returned")
+			require.Equal(t, value, model.SampleValue(0))
+		})
+
+		It("should retrieve data from different tenants via multitenant URL", Label("gke", "id=7e075898-f6c4-49d5-9d7f-8a6163759065"), func(ctx context.Context) {
+			By("Inserting data into tenant 0")
+			tenantOneInsertURL := fmt.Sprintf("http://%s/insert/0/prometheus/api/v1/write", consts.VMInsertHost(vmNamespace))
+			ts := remotewrite.GenTimeSeries("foo", 10, 1)
+			err := remotewrite.RemoteWrite(c, ts, tenantOneInsertURL)
+			require.NoError(t, err)
+
+			By("Inserting data into tenant 1")
+			tenantTwoInsertURL := fmt.Sprintf("http://%s/insert/1/prometheus/api/v1/write", consts.VMInsertHost(vmNamespace))
+			ts = remotewrite.GenTimeSeries("bar", 10, 5)
+			err = remotewrite.RemoteWrite(c, ts, tenantTwoInsertURL)
+			require.NoError(t, err)
+
+			time.Sleep(30 * time.Second)
+
+			By("Verifying data can be retrieved via multitenant URL")
+			multitenantSelectURL := fmt.Sprintf("%s/select/multitenant/prometheus", consts.VMSelectUrl(vmNamespace))
+			multitenantProm, err := promquery.NewPrometheusClient(multitenantSelectURL)
+			multitenantProm.Start = overwatch.Start
+			require.NoError(t, err)
+			value, err := multitenantProm.VectorValue(ctx, "foo_2")
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(1))
+			value, err = multitenantProm.VectorValue(ctx, "bar_2")
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(5))
 		})
 
 		It("should retrieve data from different tenants via multitenant URL", Label("gke", "id=7e075898-f6c4-49d5-9d7f-8a6163759065"), func(ctx context.Context) {
